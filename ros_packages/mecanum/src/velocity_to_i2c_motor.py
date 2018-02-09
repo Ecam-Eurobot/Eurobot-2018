2dad09273ff87bd4f072893e666d5f3a88ee50e5
Mathieu David <mathieudavid@mathieudavid.org>
2018-02-09 16:21:47 +0100

    use the data in the ROS float object

--- ros_packages/mecanum/src/velocity_to_i2c_motor.py
+++ ros_packages/mecanum/src/velocity_to_i2c_motor.py
@@ -8,20 +8,21 @@ from i2c import I2C
 pub = None
 
 def motor_front_left(vel):
-    print vel
-    robot_i2c.send(vel)
+    print vel.data
+    print type(vel.data)
+    robot_i2c.send(vel.data)
 
 def motor_front_right(vel):
-    print vel
-    robot_i2c.send(vel)
+    print vel.data
+    robot_i2c.send(vel.data)
 
 def motor_back_left(vel):
-    print vel
-    robot_i2c.send(vel)
+    print vel.data
+    robot_i2c.send(vel.data)
 
 def motor_back_right(vel):
-    print vel
-    robot_i2c.send(vel)
+    print vel.data
+    robot_i2c.send(vel.data)
 
 if __name__ == '__main__':
     try:
