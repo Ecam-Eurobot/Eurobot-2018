f91c468de1b23eee111305ec187a95e0f04670b7
Mathieu David <mathieudavid@mathieudavid.org>
2018-02-18 18:47:26 +0100

    Add functions to convert bytes to int and long

--- arduino/ecamlib/ecamlib.c
+++ arduino/ecamlib/ecamlib.c
@@ -13,3 +13,29 @@ float bytesToFloat(unsigned char b0, unsigned char b1, unsigned char b2, unsigne
 
     return number.f;
 }
+
+int bytesToInt(unsigned char b0, unsigned char b1) {
+    union {
+        int i;
+        unsigned char b[2];
+    } number;
+
+    number.b[0] = b0;
+    number.b[1] = b1;
+
+    return number.i;
+}
+
+long bytesToLong(unsigned char b0, unsigned char b1, unsigned char b2, unsigned char b3) {
+    union {
+        long l;
+        unsigned char b[4];
+    } number;
+
+    number.b[0] = b0;
+    number.b[1] = b1;
+    number.b[2] = b2;
+    number.b[3] = b3;
+
+    return number.l;
+}
