d550a437d84fdbd679455de47bd71002b8a70690
Mathieu David <mathieudavid@mathieudavid.org>
2018-02-18 18:47:26 +0100

    Add functions to convert bytes to int and long

--- arduino/ecamlib/ecamlib.h
+++ arduino/ecamlib/ecamlib.h
@@ -24,6 +24,21 @@ extern "C" {
     */
     float bytesToFloat(unsigned char b0, unsigned char b1, unsigned char b2, unsigned char b3);
 
+    /*
+        bytesToInt expects an array of two bytes and
+        converts them to a int value. Because on the
+        Arduino Uno ints are 16-bits.
+    */
+    int bytesToInt(unsigned char b0, unsigned char b1);
+
+    /*
+        bytesToLong expects an array of four bytes and
+        converts them to a long value. Because on the
+        Arduino Uno longs are 32-bits.
+    */
+    long bytesToLong(unsigned char b0, unsigned char b1, unsigned char b2, unsigned char b3);
+
+
 #ifdef __cplusplus
 }
 #endif
