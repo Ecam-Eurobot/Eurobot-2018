6df38fd820cc53660fd78a0be943cbcc4d0f7362
bluebeel <saikouah@gmail.com>
2018-03-22 14:14:20 +0100

    Merge ros-move-base and update minus config

--- ros_packages/minus_navigation/src/nav_goals.py
+++ ros_packages/minus_navigation/src/nav_goals.py
@@ -21,11 +21,7 @@ class MoveBaseTest:
         goal_states = ['PENDING', 'ACTIVE', 'PREEMPTED', 'SUCCEEDED', 'ABORTED', 'REJECTED',
                        'PREEMPTING', 'RECALLING', 'RECALLED', 'LOST']
 
-        locations = dict()
-        locations['first'] = Pose(Point(2.50, -0.5, 0.0), Quaternion(quaternion_from_euler(0, 0, 3.14159)))
-        #locations['second'] = Pose(Point(1.50, -1.5, 0.0), Quaternion(0.000, 0.000, 0.223, 0.975))
-        #locations['third'] = Pose(Point(1.00, -1.5, 0.0), Quaternion(0.000, 0.000, 0.223, 0.975))
-        #locations['fourth'] = Pose(Point(0.50, -0.5, 0.0), Quaternion(0.000, 0.000, 0.223, 0.975))
+        obj = Pose(Point(2.50, -0.5, 0.0), Quaternion(quaternion_from_euler(0.0, 0.0, 3.14159, axes='sxyz')))
 
         # Subscribe to the move_base action server
         self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
@@ -33,7 +29,10 @@ class MoveBaseTest:
         rospy.loginfo("Waiting for move_base action server...")
 
         # Wait 60 seconds for the action server to become available
-        self.move_base.wait_for_server(rospy.Duration(60))
+        wait = self.move_base.wait_for_server(rospy.Duration(60))
+        if not wait:
+            rospy.logerr("Action server not available!")
+            rospy.signal_shutdown("Action server not available...")
 
         rospy.loginfo("Connected to move base server")
 
@@ -41,112 +40,43 @@ class MoveBaseTest:
         # the user in RViz
         initial_pose = PoseWithCovarianceStamped()
         initial_pose.header.frame_id = "map"
-        initial_pose.pose = Pose(Point(1.00, -1.00, 0.0), Quaternion(quaternion_from_euler(0, 0, 3.14159)))
-        # Variables to keep track of success rate, running time,
-        # and distance traveled
-        n_locations = len(locations)
-        n_goals = 0
-        n_successes = 0
-        i = n_locations
-        distance_traveled = 0
-        start_time = rospy.Time.now()
-        running_time = 0
-        location = ""
-        last_location = ""
+        initial_pose.pose = Pose(Point(1.00, -1.00, 0.0), Quaternion(quaternion_from_euler(0.0, 0.0, 3.14159, axes='sxyz')))
 
-        # Get the initial pose from the user
-        # rospy.loginfo("*** Click the 2D Pose Estimate button in RViz to set the robot's initial pose...")
-        # rospy.wait_for_message('initialpose', PoseWithCovarianceStamped)
-        self.last_location = Pose()
         start = rospy.Publisher('start', String, queue_size=2)
         rospy.sleep(3)
         start.publish("Start")
         pub = rospy.Publisher('initialpose', PoseWithCovarianceStamped, queue_size=1)
         pub.publish(initial_pose)
 
-        # Make sure we have the initial pose
-        #while initial_pose.header.stamp == "":
-            #rospy.sleep(1)
-
         rospy.loginfo("Starting navigation test")
-        #rospy.sleep(5)
-        # Begin the main loop and run through a sequence of locations
-        while not rospy.is_shutdown():
-            # If we've gone through the current sequence,
-            # start with a new random sequence
-            if i == n_locations:
-                i = 0
-                sequence = sample(locations, n_locations)
-                # Skip over first location if it is the same as
-                # the last location
-                if sequence[0] == last_location:
-                    i = 1
+        # Set up the next goal location
+        self.goal = MoveBaseGoal()
+        self.goal.target_pose.pose = obj
+        self.goal.target_pose.header.frame_id = 'map'
+        self.goal.target_pose.header.stamp = rospy.Time.now()
 
-            # Get the next location in the current sequence
-            location = sequence[i]
+        # Let the user know where the robot is going next
+        rospy.loginfo("Going to: " + str(obj))
 
-            # Keep track of the distance traveled.
-            # Use updated initial pose if available.
-            if initial_pose.header.stamp == "":
-                distance = sqrt(pow(locations[location].position.x -
-                                    locations[last_location].position.x, 2) +
-                                pow(locations[location].position.y -
-                                    locations[last_location].position.y, 2))
+        # Start the robot toward the next location
+        self.move_base.send_goal(self.goal)
+
+        # Allow 5 minutes to get there
+        finished_within_time = self.move_base.wait_for_result(rospy.Duration(300))
+
+        # Check for success or failure
+        if not finished_within_time:
+            self.move_base.cancel_goal()
+            rospy.loginfo("Timed out achieving goal")
+        else:
+            state = self.move_base.get_state()
+            if state == GoalStatus.SUCCEEDED:
+                rospy.loginfo("Goal succeeded!")
+                rospy.loginfo("State:" + str(state))
             else:
-                rospy.loginfo("Updating current pose.")
-                distance = sqrt(pow(locations[location].position.x -
-                                    initial_pose.pose.pose.position.x, 2) +
-                                pow(locations[location].position.y -
-                                    initial_pose.pose.pose.position.y, 2))
-                initial_pose.header.stamp = ""
+                rospy.loginfo("Goal failed with error code: " + str(goal_states[state]))
 
-            # Store the last location for distance calculations
-            last_location = location
-
-            # Increment the counters
-            i += 1
-            n_goals += 1
-
-            # Set up the next goal location
-            self.goal = MoveBaseGoal()
-            self.goal.target_pose.pose = locations[location]
-            self.goal.target_pose.header.frame_id = 'map'
-            self.goal.target_pose.header.stamp = rospy.Time.now()
-
-            # Let the user know where the robot is going next
-            rospy.loginfo("Going to: " + str(location))
-
-            # Start the robot toward the next location
-            self.move_base.send_goal(self.goal)
-
-            # Allow 5 minutes to get there
-            finished_within_time = self.move_base.wait_for_result(rospy.Duration(300))
-
-            # Check for success or failure
-            if not finished_within_time:
-                self.move_base.cancel_goal()
-                rospy.loginfo("Timed out achieving goal")
-            else:
-                state = self.move_base.get_state()
-                if state == GoalStatus.SUCCEEDED:
-                    rospy.loginfo("Goal succeeded!")
-                    n_successes += 1
-                    distance_traveled += distance
-                    rospy.loginfo("State:" + str(state))
-                else:
-                    rospy.loginfo("Goal failed with error code: " + str(goal_states[state]))
-
-            # How long have we been running?
-            running_time = rospy.Time.now() - start_time
-            running_time = running_time.secs / 60.0
-
-            # Print a summary success/failure, distance traveled and time elapsed
-            rospy.loginfo("Success so far: " + str(n_successes) + "/" +
-                          str(n_goals) + " = " +
-                          str(100 * n_successes / n_goals) + "%")
-            rospy.loginfo("Running time: " + str(trunc(running_time, 1)) +
-                          " min Distance: " + str(trunc(distance_traveled, 1)) + " m")
-            rospy.sleep(self.rest_time)
+        rospy.sleep(self.rest_time)
 
     def update_initial_pose(self, initial_pose):
         self.initial_pose = initial_pose
