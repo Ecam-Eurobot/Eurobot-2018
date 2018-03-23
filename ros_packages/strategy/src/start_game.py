b9bc48512c208bd9178658aa9c90a13b78c3d373
bluebeela <saikouah@gmail.com>
2018-03-23 10:27:22 +0100

    Add reset func and respawn node for game

--- ros_packages/strategy/src/start_game.py
+++ ros_packages/strategy/src/start_game.py
@@ -10,12 +10,18 @@ def end_of_game_cb(event):
 
 def start(data):
     # start the timer of the game
+    rospy.loginfo("Start of game")
     rospy.Timer(rospy.Duration(rospy.get_param("/game/duration")), end_of_game_cb, oneshot=True)
+
+def reset(data):
+    rospy.loginfo("Reset of game")
+    rospy.shutdown()
 
 if __name__ == '__main__':
     try:
         rospy.init_node('game')
         rospy.Subscriber('start', Empty, start)
+        rospy.Subscriber('reset', Empty, reset)
         stop_pub = rospy.Publisher('stop', Empty, queue_size=1)
         rospy.spin()
     except rospy.ROSInterruptException:
