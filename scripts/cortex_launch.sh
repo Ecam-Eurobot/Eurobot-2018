0267bc3a694951577c5e76e0d6a3fb20d0523fb6
Mathieu David <mathieudavid@mathieudavid.org>
2018-03-24 19:17:35 +0100

    Startup script

---
+++ scripts/cortex_launch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Launch pigpio deamon
+sudo pigpiod
+
+# Export ROS env variables
+export ROS_IP=$(hostname -I)
+export ROS_MASTER_URI=http://$(hostname):11311
+
+# Launch ROS
+roslaunch robot_2018 cortex.launch
